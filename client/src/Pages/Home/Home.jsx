ed7aab50e275f73a4a0b06e63b872ea2fd4f52cd
ZAIN UL ABIDIN <zainansarp41@gmail.com>
2023-07-30 19:12:08 +0500

    recent trips added

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -4,14 +4,15 @@ import Hero from '../../Components/heroSection/Hero'
 import homeHero from '../../Assets/homeHero.jpg'
 
 import ServiceSec from '../../Components/ServicesSection/ServiceSec'
+import Trip from '../../Components/trip/Trip'
 
 export default function Home() {
     return (
         <>
             <Hero himg={homeHero} htag={"hero_tag"} hbtn={"BTN"} head={"Start Yout Journey with us"} hdesc={"we offer best pakage for traveling and exploring world"} />
             <PopularDest />
-            
             <ServiceSec />
+            <Trip />
         </>
     )
 }
